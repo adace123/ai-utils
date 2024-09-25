ea4a9d686976f15ec8418b4061270bd95c7d5e34
aaron.feigenbaum <aaron@enervee.com>
2024-09-25 12:46:18 -0700

    update deps and flake

--- flake.nix
+++ flake.nix
@@ -25,18 +25,9 @@
         projectDir = ./.;
         python = pkgs.python311;
         preferWheels = true;
-        overrides = defaultPoetryOverrides.extend (final: prev: rec {
+        overrides = defaultPoetryOverrides.extend (final: prev: {
           pyee = prev.pyee.overridePythonAttrs (old: {
             postPatch = "";
-          });
-          setuptools = prev.setuptools.overridePythonAttrs (old: rec {
-            version = "70.0.0";
-            src = pkgs.fetchFromGitHub {
-              owner = "pypa";
-              repo = "setuptools";
-              rev = "refs/tags/v${version}";
-              hash = "sha256-LXF3R9zfWylvihP2M8N94/IlgifwxUwKJFhtvcXEPB0=";
-            };
           });
         });
       };
